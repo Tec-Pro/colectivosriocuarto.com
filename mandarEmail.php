e9dd47a8bb5bc0dbbac5b05ff2e765caf55c2fa3
jjaimez <jaimezjacinto@gmail.com>
2014-09-13 11:45:51 -0300

    arreglo enviarEmail

--- mandarEmail.php
+++ mandarEmail.php
@@ -5,7 +5,7 @@
 		<meta charset="YTF-8">
 		<meta name="description" content="Colectivos de Río Cuarto Se trata de una aplicación sencilla y fácil de usar la cual contiene todos los horarios y recorridos de todas las lineas urbanas e interurbanas de la Ciudad de Río Cuarto, Córdoba, Argentina.">
 		<meta name="keywords" contenct="HTML5, CSS3, Javascript, PHP">
-		<link rel="stylesheet" href="css/acercaDe.css">	
+		<link rel="stylesheet" href="css/enviarEmail.css">	
 	</head>	
 	<body>
 		<div id="agrupar">
@@ -15,8 +15,8 @@
 				<li><a href="home.html">inicio</a></li>
 				<li><a href="horario.html">Horarios</a></li>
 				<li><a href="recorrido.html">Recorridos</a></li>
-				<li>Acerca de</li>
-				<li><a href="contacto.html">Contacto</a></li>
+				<li><a href="acercaDe.html">Acerca de</li>
+				<li>Contacto</li>
 			</ul>
 		</nav>
 		<section id="texto">
@@ -31,7 +31,8 @@ function recogerDato($campo){
 	$para="softwaretecpro@gmail.com";
 	$mensaje="Datos del formulario de contacto\n". "nombre: ".$nombre." \n"."email: ".$email." \n"."Mensaje ".$message;
 	mail($para,$asunto,$mensaje);
-	echo "<p>mensaje enviado correctamente</p>";
+	echo "<p>¡Gracias por contactar con nosotros!</p>";
+	echo "<p>Su solicitud ha sido enviada, en breve nos pondremos en contacto con usted.</p>";
 ?>
 </section>		
 		</div>	
